53a8fce4032ae260294573097108ab3eef8c8cc4
Antonin Bourguignon <antonin.bourguignon@gmail.com>
2016-04-20 00:52:23 +0200

    Day 03: step 2

--- day03/solve.py
+++ day03/solve.py
@@ -3,20 +3,29 @@ from collections import Counter
 from data import input
 
 
-position = (0,0)  # Initial position
+input_per_santa = {
+    'Santa': input[0::2],
+    'Robo-Santa': input[1::2],
+}
+
 positions = []
-positions.append(position)
-for move in input:
-    if move == '^':
-        position = (position[0], position[1]+1)
-    elif move == '>':
-        position = (position[0]+1, position[1])
-    elif move == 'v':
-        position = (position[0], position[1]-1)
-    elif move == '<':
-        position = (position[0]-1, position[1])
-    positions.append(position)
+for santa, plan in input_per_santa.items():
+    position = (0,0)  # Initial position
+    positions_per_santa = []
+    positions_per_santa.append(position)
+    for move in plan:
+        if move == '^':
+            position = (position[0], position[1]+1)
+        elif move == '>':
+            position = (position[0]+1, position[1])
+        elif move == 'v':
+            position = (position[0], position[1]-1)
+        elif move == '<':
+            position = (position[0]-1, position[1])
+        positions_per_santa.append(position)
 
-c = Counter(positions)
+    print 'Number of houses that received at least one present from %s: %d' % (santa, len(Counter(positions_per_santa)))
 
-print 'Number of houses that received at least one present from Santa: %d' % len(c)
+    positions.extend(positions_per_santa)
+
+print 'Number of houses that received at least one present from the Santa team: %d' % len(Counter(positions))
