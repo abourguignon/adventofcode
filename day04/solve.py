27c6562e3579470dd6a23e7118a777bea92281c4
Antonin Bourguignon <antonin.bourguignon@gmail.com>
2016-05-03 23:14:57 +0200

    Day 04: step 2

--- day04/solve.py
+++ day04/solve.py
@@ -3,11 +3,17 @@ from hashlib import md5
 from data import input
 
 
-i = 0
-md5hash = ''
-while not md5hash.startswith('00000'):
-    i+=1
-    key = input + str(i)
-    md5hash = str(md5(key).hexdigest())
+def mine_advent_coin(key, leading_sequence):
+    i = 0
+    md5hash = ''
+    while not md5hash.startswith(leading_sequence):
+        i+=1
+        key = input + str(i)
+        md5hash = str(md5(key).hexdigest())
 
-print 'Answer: %s, because md5(%s).hexdigest() => %s' % (i, key, md5hash)
+    print 'Answer: %s, because md5(%s).hexdigest() => %s' % (i, key, md5hash)
+
+    return i
+
+mine_advent_coin(input, 5*'0')
+mine_advent_coin(input, 6*'0')
